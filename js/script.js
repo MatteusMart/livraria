690fe4312bf7c5051f9df68b310023cdcebe763f
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-06-21 22:28:24 -0300

    Adicionando java script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Criando uma função em JavaScript
+// Formato ES6 baseado em Arrow Function
+
+// Função que realiza cadastro de livros 
+const cadastrar = () =>{
+
+    // captura o valor digitado no campo titulo
+    let titulo = document.getElementById('titulo').value
+
+    alert(titulo)
+}
+
+// Forma de função padrao
+// function Cadastrar(){
+// }
